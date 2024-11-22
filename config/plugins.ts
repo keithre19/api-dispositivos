9679b9639f199034195d2223d8dd541610f2798c
Keithleen Ivana Ruíz Estrada <125908174+keithre19@users.noreply.github.com>
2024-11-21 23:26:54 -0600

    Add core API routes, services, and controllers for cart, order, order-detail, and product

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,19 @@
 export default () => ({});
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: 'dffwydeim',
+          api_key: '611531695861756',
+          api_secret: 'nqOHSyfRtyLl-CL-YXZuThoEz1w',
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+    // ...
+  });
